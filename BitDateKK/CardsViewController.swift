c5a2821767c834d18fda6c6a44b7c1fd9e8259b8
krzychukula <krzychukula@gmail.com>
2015-06-24 19:51:45 +0200

    CardsViewController

---
+++ BitDateKK/CardsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CardsViewController.swift
+//  BitDateKK
+//
+//  Created by Krzysztof Kula on 24/06/15.
+//  Copyright (c) 2015 Krzysztof Kula. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+    @IBOutlet weak var cardStackView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
