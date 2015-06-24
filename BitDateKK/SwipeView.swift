59d7116e7d56ef72cb1e4ffc5dcfc2eac54525a0
krzychukula <krzychukula@gmail.com>
2015-06-24 20:10:21 +0200

    move cardview

--- BitDateKK/SwipeView.swift
+++ BitDateKK/SwipeView.swift
@@ -12,6 +12,8 @@ import UIKit
 class SwipeView: UIView {
     private let card: CardView = CardView()
     
+    private var originalPoint: CGPoint?
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
@@ -36,6 +38,8 @@ class SwipeView: UIView {
         
         self.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "dragged:"))
         
+        originalPoint = center
+        
         card.setTranslatesAutoresizingMaskIntoConstraints(false)
         
         setConstraints()
@@ -44,6 +48,8 @@ class SwipeView: UIView {
     func dragged(gestureRecogniser: UIPanGestureRecognizer) {
         let distance = gestureRecogniser.translationInView(self)
         println("Distance x: \(distance.x)  y: \(distance.y)")
+        
+        center = CGPointMake(originalPoint!.x + distance.x, originalPoint!.y + distance.y)
     }
     
     private func setConstraints() {
