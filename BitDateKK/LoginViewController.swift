bcf4d776a258c650bc35f00eb4f4cdec5c84ce82
krzychukula <krzychukula@gmail.com>
2015-06-26 08:24:08 +0200

    login view

---
+++ BitDateKK/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  BitDateKK
+//
+//  Created by Krzysztof Kula on 26/06/15.
+//  Copyright (c) 2015 Krzysztof Kula. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pressedFBLogin(sender: UIButton) {
+        
+        
+    }
+
+
+}
