a2f7748b278013d6118b477811ee5c76bbb77f62
krzychukula <krzychukula@gmail.com>
2015-06-27 15:24:29 +0200

    create profile view controller

---
+++ BitDateKK/ProfileViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileViewController.swift
+//  BitDateKK
+//
+//  Created by Krzysztof Kula on 27/06/15.
+//  Copyright (c) 2015 Krzysztof Kula. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
