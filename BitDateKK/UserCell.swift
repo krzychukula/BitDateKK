fe42b46488aea8656fd315c3bad729414cb5664e
krzychukula <krzychukula@gmail.com>
2015-06-28 11:17:35 +0200

    setup user cell

--- BitDateKK/UserCell.swift
+++ BitDateKK/UserCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class UserCell: UITableViewCell {
 
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
